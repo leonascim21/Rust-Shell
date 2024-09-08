e497f0aa913e28f88f0b57ce5887359539d4f98a
leonascim <leonascim21@gmail.com>
2024-09-08 19:33:52 -0400

    print working directory and take in input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+use std::io::Write;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    while input.trim() != "exit"
+    {
+        print_working_dir();
+        io::stdout().flush().expect("failed to flush output");
+
+        input.clear();
+        io::stdin().read_line(&mut input).expect("failed to read input");
+
+    }
+}
+
+fn print_working_dir() {
+    print!("{}", env::current_dir().unwrap().display());
 }
