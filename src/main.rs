14823c272ced8e0ca8f03548008f878488e2da21
leonascim <leonascim21@gmail.com>
2024-09-09 19:50:01 -0400

    echo command

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,33 @@ fn main() {
     let mut input = String::new();
     while input.trim() != "exit"
     {
-        print_working_dir();
+        print!("USER@MACHINE:{}> ", env::current_dir().unwrap().display());
         io::stdout().flush().expect("failed to flush output");
 
         input.clear();
         io::stdin().read_line(&mut input).expect("failed to read input");
-
         let tokens: Vec<&str> = input.trim().split_whitespace().collect();
 
-    }
 
+        if tokens[0] == "echo"
+        {
+            echo(&tokens);
+        }
+    }
 }
 
-fn print_working_dir() {
-    print!("{}> ", env::current_dir().unwrap().display());
+fn echo(input: &[&str]) {
+    if input.len() == 1
+    {
+        println!();
+        return;
+    }
+    let mut index = 1;
+    let length = input.len();
+    while index < length-1
+    {
+        print!("{} ", input[index] );
+        index += 1;
+    }
+    println!("{}", input[index]);
 }
