19605bcdfb97282df65c377d27e4c576416c3501
leonascim <leonascim21@gmail.com>
2024-09-12 16:51:49 -0400

    formatted code

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,61 @@
+use std::env;
 use std::io;
 use std::io::Write;
-use std::env;
 
 fn main() {
     let mut input = String::new();
-    while input.trim() != "exit"
-    {
-        print!("{}@{}:{}> ", get_env_variable("USER".to_string()), get_env_variable("HOSTNAME".to_string()), get_env_variable("PWD".to_string()));
+    while input.trim() != "exit" {
+        print!(
+            "{}@{}:{}> ",
+            get_env_variable("USER".to_string()),
+            get_env_variable("HOSTNAME".to_string()),
+            get_env_variable("PWD".to_string())
+        );
         io::stdout().flush().expect("failed to flush output");
 
         input.clear();
-        io::stdin().read_line(&mut input).expect("failed to read input");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read input");
         let tokens: Vec<String> = tokenize(&input);
 
-
-        if tokens[0] == "echo"
-        {
+        if tokens[0] == "echo" {
             echo(&tokens);
         }
     }
 }
 
-fn tokenize(input: &String) -> Vec<String>
-{
+fn tokenize(input: &String) -> Vec<String> {
     let tokens: Vec<&str> = input.trim().split_whitespace().collect();
     let mut result: Vec<String> = Vec::new();
-    for token in tokens
-    {
-        if token.starts_with('$')
-        {
+    for token in tokens {
+        if token.starts_with('$') {
             result.push(get_env_variable(token[1..].to_string()));
-        }
-        else if token.starts_with("~/") || (token == "~" && token.len() == 1)
-        {
+        } else if token.starts_with("~/") || (token == "~" && token.len() == 1) {
             result.push(get_env_variable("HOME".to_string()))
-        }
-        else
-        {
+        } else {
             result.push(token.to_string());
         }
     }
     result
 }
 
-fn echo(input: &Vec<String>)
-{
-    if input.len() == 1
-    {
+fn echo(input: &Vec<String>) {
+    if input.len() == 1 {
         println!();
         return;
     }
     let mut index = 1;
     let length = input.len();
-    while index < length-1
-    {
-        print!("{} ", input[index] );
+    while index < length - 1 {
+        print!("{} ", input[index]);
         index += 1;
     }
     println!("{}", input[index]);
 }
 
-fn get_env_variable(input: String) -> String
-{
-    if let Ok(output) = env::var(input)
-    {
+fn get_env_variable(input: String) -> String {
+    if let Ok(output) = env::var(input) {
         return output;
     }
     "unknown".to_string()
